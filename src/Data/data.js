90802a51204bfa6da1fef960dbbebffdcec9a401
Tommy Ren <tommyr418@gmail.com>
2019-06-03 17:54:04 -0400

    add data files

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+export default {
+  1: {
+    name: "All 3 levels",
+    price: 495,
+    credits: {
+      do: 45,
+      pa: 45,
+      nurse: 54
+    }
+  },
+  2: {
+    name: "Level 1",
+    price: 195,
+    credits: {
+      do: 15,
+      pa: 15,
+      nurse: 18
+    }
+  },
+  3: {
+    name: "Level 2",
+    price: 195,
+    credits: {
+      do: 15,
+      pa: 15,
+      nurse: 18
+    }
+  },
+  4: {
+    name: "Level 3",
+    price: 195,
+    credits: {
+      do: 15,
+      pa: 15,
+      nurse: 18
+    }
+  }
+}
